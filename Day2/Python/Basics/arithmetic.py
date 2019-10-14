24722f178fa20c72ec80cd6e512d76e12f5dbede
Matthew Gleich <matthewgleich@gmail.com>
2019-10-13 21:51:16 -0400

    Create arthmetic.py

---
+++ Day2/Python/Basics/arithmetic.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 100
+
+# Addition:
+numSum = num1 + num2
+
+# Subtraction:
+numDifference = num1 - num2
+
+# Division:
+numQuotient = num1 / num2
+
+# Multiplication:
+numProduct = num1 * num2
