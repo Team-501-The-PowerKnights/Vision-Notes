996bdea198bd7b049aca3a838b20bf60e732cce7
Matthew Gleich <matthewgleich@gmail.com>
2020-04-01 20:33:30 -0400

    🚀 Update for day 4

--- Flutter/example_app/lib/main.dart
+++ Flutter/example_app/lib/main.dart
@@ -1,40 +1,79 @@
-// Dart imports
-
-// Flutter imports
 import 'package:flutter/material.dart';
 
-// Package imports
+void main() => runApp(MyApp());
 
-// Our App imports
-import 'package:example_app/widgets/home.dart';
-
-void main() => runApp(ExampleApp());
-
-class ExampleApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      title: "Example App",
+      home: MyHomeScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class MyHomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Example App"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.place,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Colors.red,
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(30),
+                // padding: EdgeInsets.only(top: 100, right: 100),
+                // padding: EdgeInsets.symmetric(horizontal: 100),
+                child: Text(
+                  "Hello",
+                  style: TextStyle(
+                    fontSize: 40,
+                  ),
+                ),
+              ),
             ),
-            onPressed: () => print("You pressed this button!"),
-          ),
-        ],
+            MyButton(Colors.yellow, Colors.blue),
+          ],
+        ),
       ),
-      body: Home(),
+    );
+  }
+}
+
+class MyButton extends StatefulWidget {
+  final Color _color1;
+  final Color _color2;
+
+  MyButton(this._color1, this._color2);
+
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool _green = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      color: _green ? widget._color1 : widget._color2,
+      onPressed: () {
+        setState(() {
+          _green = !_green;
+        });
+      },
+      child: Text(
+        "Turn me ${_green ? "red" : "green"}",
+        style: TextStyle(
+          fontSize: 20,
+        ),
+      ),
     );
   }
 }
